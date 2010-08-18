1438af997c0ac859b1ece52bbb614f83ff310756
U-AMXDMBRH2\Matthew <Matthew@AMXDMBRH2.(none)>
2010-08-18 12:16:06 +0100

    First commit

---
+++ AmxMobile.WinMo.HelloWorld/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace AmxMobile.WinMo.HelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.buttonSayHello = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonSayHello
+            // 
+            this.buttonSayHello.Location = new System.Drawing.Point(15, 15);
+            this.buttonSayHello.Name = "buttonSayHello";
+            this.buttonSayHello.Size = new System.Drawing.Size(104, 20);
+            this.buttonSayHello.TabIndex = 0;
+            this.buttonSayHello.Text = "Say Hello";
+            this.buttonSayHello.Click += new System.EventHandler(this.buttonSayHello_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.buttonSayHello);
+            this.Menu = this.mainMenu1;
+            this.Name = "Form1";
+            this.Text = "Hello World";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSayHello;
+    }
+}
+
---
+++ AmxMobile.WinMo.HelloWorld/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AmxMobile.WinMo.HelloWorld
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSayHello_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello, world!");
+        }
+    }
+}
